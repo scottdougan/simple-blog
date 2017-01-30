76456d597dcc992e372431d5db3014b3d8cde47a
Scott Dougan <scottdougan@me.com>
2017-01-30 12:29:00 -0500

    Modified the server to return a static list of post titles

--- server.js
+++ server.js
@@ -4,12 +4,28 @@ const express = require('express'),
 
 const app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+const posts = [
+  {id: 10, title: 'Black Ash'},
+  {id: 11, title: 'Gray Birch'},
+  {id: 12, title: 'Butternut'},
+  {id: 13, title: 'Black Cherry'},
+  {id: 14, title: 'Red Spruce'},
+  {id: 15, title: 'Black Walnut'}
+];
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
 
-app.listen(3000, function () {
-  console.log('Simple Blog listening on port 3000!');
+
+app.get('/posts', function (req, res) {
+  res.send(JSON.stringify({posts: posts}));
+});
+
+app.listen(4000, function () {
+  console.log('Simple Blog listening on port 4000!');
 });
 
 
