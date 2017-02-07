faecf08b79b734de2753b766b71ca00dc108e5df
Scott Dougan <scottdougan@me.com>
2017-02-07 18:14:52 -0500

    Removing an import

--- public/app/app.module.ts
+++ public/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule }       from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BrowserModule }  from '@angular/platform-browser';
-import { FormsModule }    from '@angular/forms';
 import { HttpModule, JsonpModule }    from '@angular/http';
 
 import { AppComponent }         from './app.component';
@@ -10,6 +9,7 @@ import { AppRoutingModule }     from './app-routing.module';
 import { HomeModule } from './home/home.module';
 import { PostsModule } from './posts/posts.module';
 import { PostViewModule } from './post-view/post-view.module';
+import { PostCreateModule } from './post-create/post-create.module';
 import { SharedModule } from './shared/shared.module';
 
 @NgModule({
@@ -17,10 +17,12 @@ import { SharedModule } from './shared/shared.module';
     BrowserModule, 
     HttpModule, 
     JsonpModule,
+    // My modules
     AppRoutingModule,
     HomeModule,
     PostsModule,
     PostViewModule,
+    PostCreateModule,
     SharedModule.forRoot()
   ],
   declarations: [
