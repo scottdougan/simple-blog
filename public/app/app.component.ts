353ce72bad840f27e524c55bbdf83f77442a9899
Scott Dougan <scottdougan@me.com>
2017-02-09 11:01:13 -0500

    Update the router-outlet

--- public/app/app.component.ts
+++ public/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
     <section class="sample-app-content">
         <nav>
             <a routerLink="/home" routerLinkActive="active">Home</a>
-            <a routerLink="/createpost" routerLinkActive="active">Create Post</a>
+            <a routerLink="/create" routerLinkActive="active">Create</a>
             <a routerLink="/posts" routerLinkActive="active">All Posts</a>
         </nav>
         <router-outlet></router-outlet>
