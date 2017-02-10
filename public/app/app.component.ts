96144e86d3ee5e972b61d2a1b42cf0a4e9c741a2
Scott Dougan <scottdougan@me.com>
2017-02-10 15:42:47 -0500

    New navigation bar

--- public/app/app.component.ts
+++ public/app/app.component.ts
@@ -4,14 +4,19 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app',
     template: `
-    <section class="sample-app-content">
-        <nav>
-            <a routerLink="/home" routerLinkActive="active">Home</a>
-            <a routerLink="/create" routerLinkActive="active">Create</a>
-            <a routerLink="/posts" routerLinkActive="active">All Posts</a>
-        </nav>
-        <router-outlet></router-outlet>
-    </section>
+      <section class="sample-app-content">
+          <nav>
+            <div class="nav-wrapper">
+              <a href="#" class="brand-logo right">Simple Blog</a>
+              <ul id="nav-mobile" class="left hide-on-med-and-down">
+                <li><a routerLink="/home" routerLinkActive="active">Home</a></li>
+                <li><a routerLink="/create" routerLinkActive="active">Create</a></li>
+                <li><a routerLink="/posts" routerLinkActive="active">All Posts</a></li>
+              </ul>
+            </div>
+          </nav>
+          <router-outlet></router-outlet>
+      </section>
     `
 })
 export class AppComponent { 
